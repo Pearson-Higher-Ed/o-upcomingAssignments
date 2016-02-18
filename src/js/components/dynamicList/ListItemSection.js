a33e3f22d525a6b67db185cc44534756c3cfa418
ulantja <jason.lantz@pearson.com>
2016-02-18 02:03:21 -0700

    shorten date string in list

---
+++ src/js/components/dynamicList/ListItemSection.js
@@ -0,0 +1,32 @@
+import React, { Component, PropTypes } from 'react'
+import ListItem                        from './ListItem'
+
+
+export default class ListItemSection extends Component{
+
+
+  render(){
+
+    let { data, index }  = this.props
+    , dueDateObject      = new Date(data.due_date)
+    , dueDateString      = dueDateObject.toDateString()
+    , dueDateStringShort = dueDateString.slice(0,(dueDateString.length - 4)).trim()
+    , items              = data.items
+
+    return(
+      <li className='listItemSection' id={`listItemSection${index}`} key={`listItemSection${index}`} >
+        <h2 className='due_date' >{dueDateStringShort}</h2>
+        <ul>
+          { items.map( (d,i) => { return <ListItem d={d} i={i} key={`ListItem${i}`} /> } ) }
+        </ul>
+      </li>
+    )
+  }
+
+
+}
+
+ListItemSection.propTypes = {
+  data  : PropTypes.object.isRequired,
+  index : PropTypes.number.isRequired
+}
